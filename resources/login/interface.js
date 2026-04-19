cf05f17480e93a14033b79f70db52f8c33210044
root <azusaings@gmail.com>
2026-04-19 10:40:52 +0000

    chore: remove trash files

--- resources/login/interface.js
+++
@@ -1,8 +0,0 @@
-function click_handler() {
-    // window.location.href="info.html"
-    console.log(    document.querySelector("input[placeholder=Username]") )
-    console.log(    document.querySelector("input[placeholder=Password]") )
-
-}
-
-document.querySelector("button").addEventListener("click", click_handler)
