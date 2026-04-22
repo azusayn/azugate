37b1641fb16bb2e9f65b1118ad6873e35dda7a18
azusayn <azusaings@gmail.com>
2026-04-23 00:46:10 +0800

    chore: update

--- core/build_azugate.sh
+++ core/build_azugate.sh
@@ -1,1 +1,5 @@
-cmake -B build -DCMAKE_MAKE_PROGRAM="/usr/bin/make" --preset=default .
+cmake -B build \
+  -DCMAKE_MAKE_PROGRAM="/usr/bin/make" \
+  -DCMAKE_C_COMPILER="/usr/bin/gcc" \
+  -DCMAKE_CXX_COMPILER="/usr/bin/g++" \
+  --preset=default .
